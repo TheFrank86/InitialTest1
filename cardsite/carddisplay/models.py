8b4d9a4f1828238ca344d7be9e57b95268090d1e
Frank <thefrank86@gmail.com>
2022-08-30 13:44:44 -0500

    added pangas comments to models

--- cardsite/carddisplay/models.py
+++ cardsite/carddisplay/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+# import pandas as pd
 
 # Create your models here.
 class Question(models.Model):
@@ -10,6 +11,19 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+
+# df1 = pd.read_csv('https://api.scryfall.com/cards/search?format=csv&q=s:neo ', usecols = ['name', 'image_uri'])
+
+# <html>
+# x=0
+# for x in range of len(df1-1)
+# <div>
+#    <img src="{{df1.iloc[x, 1]}}" />
+# </div>
+# </html>
+
+# .iloc[row, column]
+# print(df1.iloc[5, 1])
 class card(models.Model):
     name = models.CharField(max_length=64)
     image = models.CharField(max_lenth=128)
