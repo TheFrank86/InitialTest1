b485bcabe441f716648961963f82d9daf4e822dd
Frank <thefrank86@gmail.com>
2022-08-30 13:59:27 -0500

    added carddisplay to apps(settings.py)

--- cardsite/cardsite/settings.py
+++ cardsite/cardsite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'carddisplay',
     'carddisplay.apps.CarddisplayConfig',
     'django.contrib.admin',
     'django.contrib.auth',
